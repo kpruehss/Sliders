914f70305f76ce49ae1163f65997cbe4cd1c84e4
Karsten Pruehss <kartug@karstens-mac-mini.local>
2015-11-12 10:53:23 +1100

    Initial Commit

---
+++ Sliders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sliders
+//
+//  Created by Karsten Pruehss on 12/11/2015.
+//  Copyright © 2015 Karsten Pruehss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
